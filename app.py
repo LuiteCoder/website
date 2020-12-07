09ed20e0ea4a741e57fb9b8c50553687c6caf58c
LuiteCoder <luitestevens3@gmail.com>
2020-12-07 17:01:30 +0100

    more stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+from jinja2 import TemplateNotFound
 
 app = flask.Flask(__name__)
 site_title = "Luite"
@@ -12,8 +13,16 @@ def home(url):
             return flask.send_from_directory('img', 'favicon.png')
         else:
             return flask.render_template(f"{url}.html", title=site_title)
-    except:
+    except TemplateNotFound:
         return flask.render_template("404.html", url=f"{flask.request.url_root}{url}", title=site_title)
+
+@app.route('/img/<path:path>')
+def img(path):
+    return flask.send_from_directory('img', path)
+
+@app.errorhandler(404)
+def site_not_found(e):
+    return flask.render_template("404.html", url=f"{flask.request.url}", title=site_title), 404
 
 @app.route("/")
 def redirect():
