65e459256b2ce7fca472bd4d9b45f1084cf04445
LuiteCoder <luitestevens3@gmail.com>
2020-12-07 16:34:39 +0100

    site coming together a bit :D

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import flask
 
 app = flask.Flask(__name__)
+site_title = "Luite"
 
 # this is so its easier. i dont wanna do url 1 by one tho.
 
-@app.route("/<url>/")
+@app.route("/<url>")
 def home(url):
     try:
         if url == "favicon.ico":
             return flask.send_from_directory('img', 'favicon.png')
         else:
-            return flask.render_template(f"{url}.html")
+            return flask.render_template(f"{url}.html", title=site_title)
     except:
-        flask.abort(404)
+        return flask.render_template("404.html", url=f"{flask.request.url_root}{url}", title=site_title)
 
 @app.route("/")
 def redirect():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html', title=site_title)
 
 
 
 if __name__ == "__main__":
-    app.run(port=81, host="0.0.0.0")
+    app.run(port=83, host="0.0.0.0")
