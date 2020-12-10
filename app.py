a198e5d08e833001d94f85365f4b93d444c8fc29
LuiteCoder <luitestevens3@gmail.com>
2020-12-10 16:02:49 +0100

    funny stuff added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import flask
+import time
+
 from jinja2 import TemplateNotFound
 
 app = flask.Flask(__name__)
@@ -29,10 +31,21 @@ def site_not_found(e):
     return flask.render_template("404.html", url=f"{flask.request.url}", title=site_title), 404
 
 @app.route("/")
-def redirect():
+def site():
     return flask.render_template('index.html', title=site_title)
 
+@app.route("/glr/")
+@app.route("/galaxylifereborn/")
+def glr():
+    return flask.redirect("https://galaxylifereborn.com/", code=302)
 
+@app.route("/discord/")
+def dc():
+    return f"<html><body><p>Redirecting...</p><script>var timer = setTimeout(function() {{window.location='/sad/'}}, 1300);</script></body></html>" 
+
+@app.route("/sad/")
+def sad():
+    return 'i have no discord server :( <iframe width="1" height="1" src="https://www.youtube.com/embed/6qpv9jbFQtg?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
 if __name__ == "__main__":
     app.run(port=81, host="0.0.0.0")
